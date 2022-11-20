ac4f1738ad2c31c651fd974adbe8c3896c24bad1
Ryan <50505980+ryan5453@users.noreply.github.com>
2022-11-20 00:35:29 -0500

    fix

--- predict.py
+++ predict.py
@@ -24,45 +24,45 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        audio: Path = Input(title="The audio file to separate"),
+        audio: Path = Input(description="The audio file to separate"),
         two_stems: str = Input(
             default=None,
-            title="If you want to separate into two stems, enter the name of the stem here.",
-            choices=["drums", "bass", "other", "vocals", "accompaniment"],
+            description="If you want to separate into two stems, enter the name of the stem here.",
+            choices=["drums", "bass", "other", "vocals"],
         ),
         int24: bool = Input(
             default=False,
-            title="If you want to output 24 bit wav files, set this to true.",
+            description="If you want to output 24 bit wav files, set this to true.",
         ),
         float32: bool = Input(
             default=False,
-            title="If you want to output 32 bit wav files, set this to true. Keep in mind this is 2x bigger.",
+            description="If you want to output 32 bit wav files, set this to true. Keep in mind this is 2x bigger.",
         ),
         clip_mode: str = Input(
             default="rescale",
-            title="trategy for avoiding clipping: rescaling entire signal if necessary (rescale) or hard clipping (clamp)",
+            description="trategy for avoiding clipping: rescaling entire signal if necessary (rescale) or hard clipping (clamp)",
             choices=["rescale", "clamp"],
         ),
         mp3: bool = Input(
             default=False,
-            title="If you want to convert the output wavs to mp3, set this to true.",
+            description="If you want to convert the output wavs to mp3, set this to true.",
         ),
         mp3_bitrate: int = Input(
-            default=320, title="The bitrate of the converted mp3."
+            default=320, description="The bitrate of the converted mp3."
         ),
         shifts: int = Input(
             default=1,
-            title="Number of random shifts for equivariant stabilization. Increase separation time but improves quality for Demucs. 10 was used in the original paper.",
+            description="Number of random shifts for equivariant stabilization. Increase separation time but improves quality for Demucs. 10 was used in the original paper.",
         ),
         workers: int = Input(
             default=0,
-            title="Number of jobs. This can increase memory usage but will be much faster when multiple cores are available.",
+            description="Number of jobs. This can increase memory usage but will be much faster when multiple cores are available.",
         ),
         split: bool = Input(
             default=False,
-            title="If you want to split audio in chunks, set this to true. This can use large amounts of memory.",
+            description="If you want to split audio in chunks, set this to true. This can use large amounts of memory.",
         ),
-        overlap: float = Input(default=0.25, title="Overlap between the splits."),
+        overlap: float = Input(default=0.25, description="Overlap between the splits."),
     ) -> DemucsResponse:
 
         self.model.cpu()
@@ -102,28 +102,27 @@ class Predictor(BasePredictor):
         if two_stems is None:
             for source, name in zip(sources, self.model.sources):
                 with tempfile.NamedTemporaryFile(suffix=f".{ext}") as f:
-                    save_audio(f.name, source, **kwargs)
-                    output_stems.append(DemucsStem(name=name, audio=File(f.name)))
+                    save_audio(source.cpu(), f.name, **kwargs)
+                    output_stems.append(DemucsStem(name=name, audio=BytesIO(open(f.name, "rb").read())))
         else:
             sources = list(sources)
 
             with tempfile.NamedTemporaryFile(suffix=f".{ext}") as f:
                 save_audio(
-                    f.name, sources[self.model.sources.index(two_stems)], **kwargs
+                    sources[self.model.sources.index(two_stems)].cpu(), f.name, **kwargs
                 )
-                output_stems.append(DemucsStem(name=two_stems, audio=File(f.name)))
+                output_stems.append(DemucsStem(name=two_stems, audio=BytesIO(open(f.name, "rb").read())))
 
             sources.pop(self.model.sources.index(two_stems))
 
-            # Warning : after poping the stem, selected stem is no longer in the list 'sources'
             other_stem = torch.zeros_like(sources[0])
             for i in sources:
                 other_stem += i
 
-            with tempfile.NamedTemporaryFile(suffix=f".{ext}", prefix="no_") as f:
-                save_audio(f.name, other_stem, **kwargs)
+            with tempfile.NamedTemporaryFile(suffix=f".{ext}") as f:
+                save_audio(other_stem.cpu(), f.name, **kwargs)
                 output_stems.append(
-                    DemucsStem(name="no_" + two_stems, audio=File(f.name))
+                    DemucsStem(name="no_" + two_stems, audio=BytesIO(open(f.name, "rb").read())))
                 )
 
         return DemucsResponse(stems=output_stems)
